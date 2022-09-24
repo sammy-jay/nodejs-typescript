5580be6aa53fdeb1fc0481976310216174705acc
sammy-jay <sammyjayden27@gmail.com>
2022-09-24 07:13:49 +0100

    learnt about how to use buffers and understood why we use them

--- index.ts
+++ index.ts
@@ -1,26 +1,52 @@
-import * as EventEmitter from 'events'
+import { StringDecoder } from 'string_decoder';
+import * as fs from 'fs';
+import * as util from 'util'
+ 
+const readFile = util.promisify(fs.readFile);
+const decoder = new StringDecoder('utf8');
 
-const eventEmitter = new EventEmitter()
+const buffer = Buffer.alloc(5)
 
-function listener (data: object) {
-	console.log(data)
-}
+buffer[0] = 123
+buffer[1] = 0
+buffer[2] = 1000000
+buffer[3] = -10000000011
+buffer[4] = 256
 
-function listener2 () {
-	console.log("Listener two")
-}
+console.log(buffer)
 
-eventEmitter.on('event', listener)
-eventEmitter.once('event2', listener2)
 
-eventEmitter.emit('event', { name: 'Tolu' })
-eventEmitter.emit('event', { name: 'Tolu' })
-eventEmitter.emit('event', { name: 'Tolu' })
+const buffer2 = Buffer.alloc(5, 1)
+console.log(buffer2)
 
-eventEmitter.removeAllListeners('event')
+const buffer3 = Buffer.from([4,5,10])
+console.log(buffer3)
 
-eventEmitter.emit('event', { name: 'Tolu' })
 
-eventEmitter.emit('event2')
-eventEmitter.emit('event2')
-eventEmitter.emit('event2')
+const strBuffer = Buffer.from("Let's rock 🚀")
+console.log(strBuffer)
+console.log(strBuffer.toString())
+
+
+ 
+const buffers = [
+  Buffer.from('Hello '),
+  Buffer.from([0b11110000, 0b10011111]),
+  Buffer.from([0b10001100, 0b10001110]),
+  Buffer.from(' world!'),
+];
+ 
+const result = buffers.reduce((result, buffer) => (
+  `${result}${decoder.write(buffer)}`
+), '');
+ 
+console.log(result); 
+
+
+ 
+readFile('package.json')
+  .then((content) => {
+    console.log(content instanceof Buffer); // true
+    console.log(content.toString())
+  })
+  .catch(error => console.log(error));
